1c6487a3fb1ffa69a143e88ce0f12ae63a2aaf8e
baicy <baicy0118@gmail.com>
2024-11-08 23:21:08 +0800

    fix: 去除不需要的引用

--- src/stores/activity.ts
+++ src/stores/activity.ts
@@ -1,36 +1,11 @@
-import { ref, computed } from 'vue'
+import { ref, computed, reactive } from 'vue'
 import { defineStore } from 'pinia'
-import {
-  getActiveSidestories,
-  getActivePools,
-  getActiveNosans,
-  getActiveDailyWorks,
-  getActiveComings
-} from '@/utils'
+import { ato } from '@/utils/utils'
+import type { Sidestory } from '@/utils/types'
+import sidestories from '@/data/sidestories.json'
 
 export const useActivityStore = defineStore('activity', () => {
-  const activityList = ref([] as any[])
-  const dailyworkList = ref([] as any[])
-  const comingList = ref([] as any[])
+  const sidestoryList = reactive(ato(sidestories, 'id')) as Record<string, Sidestory>
 
-  const activities = computed(() => {
-    activityList.value = [
-      ...getActiveSidestories(),
-      ...getActivePools(),
-      ...getActiveNosans()
-    ].sort((a, b) => a.close.diff(b.close))
-    return activityList
-  })
-
-  const dailyworks = computed(() => {
-    dailyworkList.value = [...getActiveDailyWorks()].sort((a, b) => a.close.diff(b.close))
-    return dailyworkList
-  })
-
-  const comings = computed(() => {
-    comingList.value = [...getActiveComings()].sort((a, b) => a.close.diff(b.close))
-    return comingList
-  })
-
-  return { activities, dailyworks, comings }
+  return { sidestoryList }
 })
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,3 @@
 import { getShadowColor, getTextColor, duration, len } from './utils'
-import { getActiveSidestories } from '@/utils/sidestories'
-import { getActivePools } from '@/utils/pools'
-import { getActiveNosans } from '@/utils/nosans'
-import { getActiveDailyWorks } from './dailyworks'
-import { getActiveComings } from './comings'
 
-export {
-  getShadowColor,
-  getTextColor,
-  duration,
-  len,
-  getActiveSidestories,
-  getActivePools,
-  getActiveNosans,
-  getActiveDailyWorks,
-  getActiveComings
-}
+export { getShadowColor, getTextColor, duration, len }
